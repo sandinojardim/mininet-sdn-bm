b3c37dd749e3c40a86c79951665535b010348076
root <root@ufmt.br>
2023-07-04 15:03:48 +0100

    debug

--- REST_tests/floodlight.py
+++ REST_tests/floodlight.py
@@ -5,7 +5,7 @@ REST_PORT = '8081'
 
 url1 = f'http://{CONTROLLER_IP}:{REST_PORT}/wm/core/controller/switches/json'
 url2 = f'http://{CONTROLLER_IP}:{REST_PORT}/wm/topology/links/json'
-url3 = f'http://{CONTROLLER_IP}:{REST_PORT}/wm/topology/device/json'
+url3 = f'http://{CONTROLLER_IP}:{REST_PORT}/wm/device/'
 try:
     response1 = requests.get(url1)
     response2 = requests.get(url2)
@@ -14,8 +14,9 @@ try:
         switches = response1.json()
         links = response2.json()
         hosts = response3.json()
+        host_count = len(hosts['devices'])
         print(hosts)
-        print(f'Topo = {len(switches)} | Links = {len(links)*2} | Hosts = {len(hosts)}')
+        print(f'Topo = {len(switches)} | Links = {len(links)*2} | Hosts = {hosts}')
     else:
         print(f"Error: {response1.status_code} - {response1.text}")
 except requests.exceptions.RequestException as e:
