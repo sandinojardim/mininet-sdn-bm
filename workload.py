322915d1afa4c1fa1b326f4ac78065819060eb65
root <root@ufmt.br>
2023-07-12 13:04:53 +0100

    errobobo2

--- workload.py
+++ workload.py
@@ -203,7 +203,6 @@ if __name__ == '__main__':
                 continue
             end_time = time.time()
             sum_times_on += (end_time - start_time)
-            print(sum_times_on)
             print(f'host on time_{i} = {end_time - start_time}')
 
             start_time = time.time()
